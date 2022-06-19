0acb40fc9f230767e1861c8274cc07c0208c5b55
@MariaWhitch <dontworrybehappymary@gmail.com>
2022-06-19 14:10:17 +0400

    3class

---
+++ src/main/java/pojo/Cat.java
@@ -0,0 +1,13 @@
+package pojo;
+
+public class Cat {
+    private String massage;
+
+    public String getMassage() {
+        return massage;
+    }
+
+    public void setMassage(String massage) {
+        this.massage = massage;
+    }
+}
